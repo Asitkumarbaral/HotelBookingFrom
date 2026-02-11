819a14f60e660be8b3b055293dbfc2c8962d6dcb
Asit <asitkumarbaral8@gmail.com>
2026-02-11 12:40:47 +0530

    frontend is ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoDb from "./db.js";          
- 
+import cors from "cors";
 import bookingRoute from "./routes/bookingRoute.js";
 
 const PORT = 5000;
@@ -9,7 +9,8 @@ const PORT = 5000;
 const app = express();
  mongoDb;
 app.use(express.json());
-app.use(bookingRoute);
+app.use(cors());
+app.use("/api/bookings",bookingRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ frontend/script.js
@@ -0,0 +1,63 @@
+const form = document.getElementById("bookingForm");
+const checkIn = document.getElementById("checkIn");
+const checkOut = document.getElementById("checkOut");
+const successMessage = document.getElementById("successMessage");
+
+
+const today = new Date().toISOString().split("T")[0];
+checkIn.setAttribute("min", today);
+
+
+checkIn.addEventListener("change", () => {
+    checkOut.value = "";
+    checkOut.setAttribute("min", checkIn.value);
+});
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    
+
+    document.querySelectorAll(".error").forEach(el => el.textContent = "");
+    successMessage.textContent = "";
+
+   
+
+    const bookingData = {
+        checkInDate: checkIn.value,
+        checkOutDate: checkOut.value,
+        roomType: document.getElementById("roomType").value,
+        adults: document.getElementById("adults").value,
+        children: document.getElementById("children").value,
+        specialRequests: document.getElementById("specialRequests").value
+    };
+
+    try {
+        const response = await fetch("http://localhost:5000/api/bookings/book", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(bookingData)
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            window.location.href = "Sucess.html";
+           
+        } else {
+            successMessage.textContent = data.message || "Booking failed";
+        }
+
+    } catch (error) {
+        successMessage.textContent = "Server error. Try again.";
+    }
+});
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.style.display =
+        navMenu.style.display === "flex" ? "none" : "flex";
+});
